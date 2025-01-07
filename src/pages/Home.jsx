2e2da0934f250bb2a756c072f16998b6a80db464
akansha <akanshagangwani555@gmail.com>
2025-01-07 12:13:26 +0530

    size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ const BenefitCard = ({ headingText }) => (
                       headingText === "Custom Game Components" ? "#adb5bd" :
                       "#e64980",
       boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-      width: "325px",  // Adjust width
-      height: "320px"  // Adjust height
+      width: "250px",  // Adjust width
+      height: "250px"  // Adjust height
     }}
   >
     <h3 
